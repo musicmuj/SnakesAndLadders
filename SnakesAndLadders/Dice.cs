e1eb0a8a7bfc4207882e38eb4472a97f970200ad
Karen Cheng <karen.cheng@xuenn.com>
2018-07-09 11:46:10 +0800

    Add player and dice
    Add player and dice, implement for future use

---
+++ SnakesAndLadders/Dice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLadders
+{
+    public class Dice
+    {
+        private Random _randomSvc;
+
+        public Dice()
+        {
+            _randomSvc = new Random();
+        }
+
+        public int Shake()
+        {
+            return _randomSvc.Next(6) + 1;
+        }
+    }
+}
---
+++ SnakesAndLadders/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLadders
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Square { get; set; }
+
+        public Player(string name, int currentSquare = 0)
+        {
+            this.Name = name;
+            this.Square = currentSquare;
+        }
+    }
+}
