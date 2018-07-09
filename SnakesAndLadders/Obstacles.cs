eef1c49af46dcfc60a1e2dc6b092a62cb05d35cc
Karen Cheng <musicmuj@gmail.com>
2018-07-09 19:08:16 +0800

    Fix bugs
    Ladder and snake wrong

--- SnakesAndLadders/Obstacles.cs
+++ SnakesAndLadders/Obstacles.cs
@@ -37,9 +37,43 @@ namespace SnakesAndLadders
         }
 
         public int GetResultSquare() => _sqaure;
+
+        protected void EncounterAlert(string obstacleName) => Console.WriteLine($"--- [{obstacleName}] Encountered!");
     }
 
     public class Snakes : Obstacles
+    {
+        private readonly Dictionary<int, int> _obstacles = new Dictionary<int, int>()
+        {
+            {99, 80},
+            {95, 75},
+            {92, 88},
+            {89, 68},
+            {74, 53},
+            {64, 60},
+            {62, 19},
+            {49, 11},
+            {46, 25},
+            {16, 6}
+        };
+
+        public Snakes(Obstacles nextObstacles) : base(nextObstacles)
+        {
+        }
+
+        protected override int GetNewSquare(int distince)
+        {
+            if (_obstacles.ContainsKey(distince))
+            {
+                EncounterAlert("Snake");
+                return _obstacles[distince];
+            }
+
+            return distince;
+        }
+    }
+
+    public class Ladders : Obstacles
     {
         private readonly Dictionary<int, int> _obstacles = new Dictionary<int, int>()
         {
@@ -56,35 +90,6 @@ namespace SnakesAndLadders
             {87, 94}
         };
 
-        public Snakes(Obstacles nextObstacles) : base(nextObstacles)
-        {
-        }
-
-        protected override int GetNewSquare(int distince)
-        {
-            if (_obstacles.ContainsKey(distince))
-                return _obstacles[distince];
-
-            return distince;
-        }
-    }
-
-    public class Ladders : Obstacles
-    {
-        private readonly Dictionary<int, int> _obstacles = new Dictionary<int, int>()
-        {
-            {99, 80},
-            {95, 75},
-            {92, 88},
-            {89, 68},
-            {74, 53},
-            {64, 60},
-            {62, 19},
-            {49, 11},
-            {46, 25},
-            {16, 6}
-        };
-
         public Ladders(Obstacles nextObstacles) : base(nextObstacles)
         {
         }
@@ -92,7 +97,10 @@ namespace SnakesAndLadders
         protected override int GetNewSquare(int distince)
         {
             if (_obstacles.ContainsKey(distince))
+            {
+                EncounterAlert("Ladder");
                 return _obstacles[distince];
+            }
 
             return distince;
         }
